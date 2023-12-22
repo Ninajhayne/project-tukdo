bc1b5f29c499061a3fb1230335bff1c360791e70
Niña Jhayne <ninajhaynecolar.boral@bicol-u.edu.ph>
2023-12-22 17:15:24 +0800

    add icon

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -6,6 +6,7 @@ import {
     AtSign,
     BarChart,
     BarChart3,
+    Book,
     CalendarDays,
     Check,
     ChevronDown,
@@ -66,6 +67,8 @@ import {
     type LucideProps,
     Bell,
     CreditCardIcon,
+    Users,
+    Library,
   } from "lucide-react"
 
   
@@ -76,6 +79,7 @@ import {
   import { PiBookOpenTextLight } from "react-icons/pi";
   import { RxBarChart } from "react-icons/rx";
   import { TfiCamera, TfiMusic } from "react-icons/tfi";
+  import { FcGoogle } from "react-icons/fc";
   
   export type Icon = LucideIcon
   
@@ -145,6 +149,9 @@ import {
     barChart: BarChart,
     at: AtSign,
     mentorProfile: VenetianMask,
+    book: Book,
+    usergroup: Users,
+    library: Library,
 
     academicsCategory: PiBookOpenTextLight,
     businessCategory: RxBarChart,
@@ -179,27 +186,11 @@ import {
         ></path>
       </svg>
     ),
-    google: ({ ...props }: LucideProps) => (
-      <svg
-        aria-hidden="true"
-        focusable="false"
-        data-prefix="fab"
-        data-icon="discord"
-        role="img"
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 488 512"
-        {...props}
-      >
-        <path
-          fill="currentColor"
-          d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z"
-        ></path>
-      </svg>
-    ),
+    google: FcGoogle,
     facebook: ({ ...props }: LucideProps) => (
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" {...props}>
         <path
-          fill="currentColor"
+          fill="#1877F2"
           d="M279.14 288l14.22-92.66h-88.91v-60.13c0-25.35 12.42-50.06 52.24-50.06h40.42V6.26S260.43 0 225.36 0c-73.22 0-121.08 44.38-121.08 124.72v70.62H22.89V288h81.39v224h100.17V288z"
         />
       </svg>
