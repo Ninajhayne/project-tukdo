453ca94b6fbfbb3c04ad222bc654ec01475ea702
Niña Jhayne <ninajhaynecolar.boral@bicol-u.edu.ph>
2023-12-23 22:34:54 +0800

    update

--- src/app/(auth)/(routes)/sign-up/page.tsx
+++ src/app/(auth)/(routes)/sign-up/page.tsx
@@ -45,7 +45,7 @@ export default async function SignUpPage() {
                         <div className="absolute inset-0 flex items-center">
                         <span className="w-full border-t" />
                         </div>
-                        <div className="relative flex justify-center text-xs uppercase">
+                        <div className="relative flex justify-center text-xs">
                         <span className="bg-background px-2 text-muted-foreground">
                             Or continue with
                         </span>
