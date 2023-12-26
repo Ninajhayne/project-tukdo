3a768daa5942d83b14d60b95bff06e64303c07aa
Niña Jhayne <ninajhaynecolar.boral@bicol-u.edu.ph>
2023-12-26 22:28:33 +0800

    update course list page

--- src/components/course/course-card.tsx
+++ src/components/course/course-card.tsx
@@ -53,15 +53,36 @@ export const CourseCard = ({
                         </div>
                     */}
                 </div>
-                <div className="flex flex-col pt-2">
+                <div className="flex flex-col p-4 gap-2">
+                    <div className="flex flex-col gap-x-2 justify-between">
+                        <div className="flex items-center gap-x-2">
+                            <p className="text-lg md:text-base font-medium line-clamp-2">
+                                {title}
+                            </p>
+                            <p className="ml-auto flex-shrink-0">
+                                {!rating ? "" : <span><span className="text-[#FFE600]">★</span> {rating.toFixed(1)}</span>}
+                            </p>
+                        </div>
+                    </div>
+                    
+                    <div className="flex items-center gap-x-2 text-sm md:text-xs">
+                        <div className="flex items-center gap-x-1 text-muted-foreground">
+                            <p className="text-xs px-2 rounded-md  bg-transparent text-[#00528A] border border-[#00528A] hover:bg-accent">
+                                {category}
+                            </p>
+                        </div>
+                    </div>
+{/*
                     <div className="text-lg md:text-base font-medium truncate">
                         {title}
                     </div>
-                    <p className="text-xs text-muted-foreground">
+                    <p className="text-xs px-2 rounded-md  bg-transparent text-[#00528A] border border-[#00528A] hover:bg-accent">
                         {category}
                     </p>
-                    <div className="my-3 flex items-center gap-x-2 text-sm md:text-xs">
-                        <div className="flex items-center gap-x-1 text-slate-500 space-x-1">
+
+                    */}
+                    <div className="flex items-center gap-x-2 text-sm md:text-xs">
+                        <div className="flex items-center gap-x-1 text-muted-foreground space-x-1">
                             <IconBadge size="sm" icon={BookOpen} />
                             <span>
                                 {chaptersLength} {chaptersLength === 1 ? "Chapter" : "Chapters"}
@@ -76,10 +97,12 @@ export const CourseCard = ({
                         />
                     ) : (
                         <div>
+                            {/*
                             <p className="font-medium">
-                                {!rating ? "" : `★ ${rating.toFixed(2)}`}
+                                {!rating ? "" : `★ ${rating.toFixed(1)}`}
                             </p>
-                            <p className="text-md md:text-sm font-meduim text-slate-400"> 
+                            */}
+                            <p className="text-lg font-bold text-[#F2602D]"> 
                                 {!price ? "Free" : `${formatPrice(price)}`}
                             </p>
                         </div>
--- src/components/course/course-list.tsx
+++ src/components/course/course-list.tsx
@@ -1,6 +1,7 @@
 import { Category, Course } from "@prisma/client";
 
 import { CourseCard } from "@/components/course/course-card";
+import Image from "next/image";
 
 type CourseWithProgressWithCategory = Course & {
     category: Category | null;
@@ -34,8 +35,19 @@ export const CoursesList = ({
                 ))}
             </div>
             {items.length === 0 && (
-                <div className="text-center text-sm text-muted-foreground mt-10 h-screen">
-                    No courses found
+                <div className="mb-8">
+                    <Image
+                        src= "/images/no-found.png"
+                        alt = "sign-in"
+                        width={100}
+                        height={100}
+                        className="mx-auto h-32 object-contain"
+                        loading="lazy"
+                        />
+                    <div className="text-center text-base">
+                        <p className="font-medium text-foreground mb-2">No courses found</p>
+                        <p className="text-muted-foreground">Try different category or keywords</p>
+                    </div>
                 </div>
             )}
         </div>
