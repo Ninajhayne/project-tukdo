193674466a72b6325a5ab7c40fcbe827537c4d5c
Niña Jhayne <ninajhaynecolar.boral@bicol-u.edu.ph>
2024-01-01 12:02:29 +0800

    update password input

--- src/components/password-input.tsx
+++ src/components/password-input.tsx
@@ -29,9 +29,10 @@ const PasswordInput = React.forwardRef<HTMLInputElement, InputProps>(
                         disabled={props.disabled}
                     >
                         {showPassword ? (
-                            <Icons.hide className="h-4 w-4" aria-hidden="true" />
-                        ) : (
                             <Icons.view className="h-4 w-4" aria-hidden="true" />
+                        ) : (
+                            <Icons.hide className="h-4 w-4" aria-hidden="true" />
+
                         )}
                         <span className="sr-only">
                             {showPassword ? "Hide password" : "Show password"}
